50b856751b86d63a81926295dcae3472171c96ae
Theo Belaire <theo.belaire@gmail.com>
2020-03-30 19:27:31 -0400

    Append new entries

--- js/todo.js
+++ js/todo.js
@@ -1,12 +1,24 @@
 let listEl = document.querySelector('#thelist');
 let clearBtn = document.querySelector('.clear');
 let saveBtn = document.querySelector('.save');
+let addItemEl = document.querySelector('#addItem');
 
-
-clearBtn.addEventListener('click', (event) => {
+clearBtn.addEventListener('click', function() {
     listEl.innerHTML = "";
 });
 
-saveBtn.addEventListener('click', () => {
+saveBtn.addEventListener('click', function() {
     alert("Can't save yet, sorry");
+});
+
+addItemEl.addEventListener('keypress', function(event)  {
+    if(event.which === 13) { // Enter
+        let newEntry = document.createElement('li');
+        let text = document.createTextNode(this.value);
+        newEntry.append(text);
+        listEl.append(newEntry);
+
+        this.value = " ";
+
+    }
 });
