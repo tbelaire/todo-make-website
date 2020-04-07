5ad74d66cdb36b8de11780e0533c0ea123e507d3
Theo Belaire <theo.belaire@gmail.com>
2020-04-06 22:02:21 -0400

    Added post button and updated js entry creation

--- js/todo.js
+++ js/todo.js
@@ -2,6 +2,7 @@ let listEl = document.querySelector('#thelist');
 let clearBtn = document.querySelector('.clear');
 let saveBtn = document.querySelector('.save');
 let loadBtn = document.querySelector('.load');
+let postBtn = document.querySelector('#post');
 let addItemEl = document.querySelector('#addItem');
 
 clearBtn.addEventListener('click', function() {
@@ -16,13 +17,38 @@ loadBtn.addEventListener('click', function() {
     listEl.innerHTML = localStorage.getItem('todoList');
 });
 
+postBtn.addEventListener('click', function() {
+    post();
+})
+
 addItemEl.addEventListener('keypress', function(event)  {
     if(event.which === 13) { // Enter
-        let newEntry = document.createElement('li');
-        let text = document.createTextNode(this.value);
-        newEntry.append(text);
-        listEl.append(newEntry);
-
-        this.value = " ";
+        post();
     }
 });
+
+function post() {
+    newEntry = todoEntry(addItemEl.value, "2020-04-30");
+    addItemEl.value = "";
+    listEl.append(newEntry);
+}
+
+function todoEntry(description, dueDate) {
+    // <li class="todoEntry">
+    // <div class="description">Profit</div>
+    // <div class="dueDate">2020-04-09<div>
+    // </li>
+    let newEntry = document.createElement('li');
+    newEntry.classList = "todoEntry";
+
+    let descriptionEl = document.createElement('div');
+    descriptionEl.classList = "description";
+    descriptionEl.append(document.createTextNode(description));
+
+    let dueDateEl = document.createElement('div');
+    dueDateEl.classList = "dueDate";
+    dueDateEl.append(document.createTextNode(dueDate))
+
+    newEntry.append(descriptionEl, dueDateEl);
+    return newEntry;
+}
